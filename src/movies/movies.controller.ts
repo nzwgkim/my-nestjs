1d4a2cd6f362ed571db578051cb2bf25d46bf0fe
Woody <nz.wgkim@gmail.com>
2022-07-19 20:30:21 +1200

    Decorator - Query - Param - Body

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, ConfigurableModuleBuilder, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,25 +6,34 @@ export class MoviesController {
     getAll(){
         return 'This will return all movies';
     }
-    
-    @Get('/:id')
+
+    @Get('search')
+    search(@Query('year') seachingYear: string) {
+      return `We are searching for a movie made after: ${seachingYear}`;
+    }
+
+    @Get(':id')
     getOne(@Param('id') moveId:string){
         return `This will return ONE movie with the id: ${moveId}.`;
     }
 
     @Post()
-    create(){
-        return `This will create a movie`
+    create(@Body() movieDate){
+        console.log(movieDate);
+        return movieDate;
     }
 
-    @Delete('/:id')
+    @Delete(':id')
     remove(@Param('id') movieId:string){
         return  `This will delete a movie of ${movieId}`
     }
 
-    @Patch('/:id')
-    patch(@Param('id') movieID:string){
-        return `This will patch from the id: ${movieID}`
+    @Patch(':id')
+    patch(@Param('id') movieID:string, @Body() updateData){
+        console.log(movieID);
+        return {
+            updatedMovie: movieID,
+            ...updateData,
+        };
     }
-
 }
